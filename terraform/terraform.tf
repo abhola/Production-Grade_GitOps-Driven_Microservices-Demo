04523c5d22062775959c351c40c8dbd9401f3a47
abhola <ankurhcl01@gmail.com>
2026-07-27 19:31:00 +0530

    add backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+
+backend "s3" {
+    bucket = "devopsdock-terraform-backend-bucket"
+    key    = "s3-backend"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
