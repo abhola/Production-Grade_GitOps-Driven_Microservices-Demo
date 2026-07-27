a315bdde53a2e546f6d0452741862f70169347ec
abhola <ankurhcl01@gmail.com>
2026-07-27 19:32:07 +0530

    add backendfile

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
 
 backend "s3" {
-    bucket = "devopsdock-terraform-backend-bucket"
+    bucket = "devopsdock1-terraform-backend-bucket"
     key    = "s3-backend"
     region = "us-east-1"
   }
